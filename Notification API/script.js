fd3f8cb1a1fbfeabd2feca6659b32e7e5103fb39
satyam-seth <satyam1998.1998@gmail.com>
2022-08-24 00:34:17 +0530

    remove setTimeout from notification api demo

--- Notification API/script.js
+++ Notification API/script.js
@@ -17,27 +17,19 @@ window.addEventListener('load', async () => {
         if (Notification?.permission === "granted") {
             // If the user agreed to get notified
             // Let's try to send ten notifications
-            let i = 0;
-            // Using an interval cause some browsers (including Firefox) are blocking notifications if there are too much in a certain time.
-            const interval = setInterval(() => {
-                // Thanks to the tag, we should only see the "Hi! 9" notification
-                const n = new Notification(`Hi! ${i}`);
 
-                // Close the notification
-                n.click = () => {
-                    if (document.visibilityState !== "visible") {
-                        // If the page is not visible, we want to focus it
-                        // so the user can see the result of the notification
-                        window.parent.focus();
-                        notification.close();
-                    }
-                }
+            // Thanks to the tag, we should only see the "Hi! 9" notification
+            const n = new Notification('Notification from Satyam Seth', { body: 'This is a notification from Satyam Seth for testing purpose', icon: 'https://avatars.githubusercontent.com/u/63374020?v=4' });
 
-                if (i === 9) {
-                    i++;
-                    clearInterval(interval);
+            // Close the notification
+            n.click = () => {
+                if (document.visibilityState !== "visible") {
+                    // If the page is not visible, we want to focus it
+                    // so the user can see the result of the notification
+                    window.parent.focus();
+                    notification.close();
                 }
-            }, 200);
+            }
         } else if (Notification && Notification.permission !== "denied") {
             // If the user hasn't told if they want to be notified or not
             // Note: because of Chrome, we are not sure the permission property
@@ -45,27 +37,16 @@ window.addEventListener('load', async () => {
             Notification.requestPermission((status) => {
                 // If the user said okay
                 if (status === "granted") {
-                    let i = 0;
-                    // Using an interval cause some browsers (including Firefox) are blocking notifications if there are too much in a certain time.
-                    const interval = setInterval(() => {
-                        // Thanks to the tag, we should only see the "Hi! 9" notification
-                        const n = new Notification(`Hi! ${i}`);
-
-                        // Close the notification
-                        n.click = () => {
-                            if (document.visibilityState !== "visible") {
-                                // If the page is not visible, we want to focus it
-                                // so the user can see the result of the notification
-                                window.parent.focus();
-                                notification.close();
-                            }
+                    const n = new Notification('Notification from Satyam Seth', { body: 'This is a notification from Satyam Seth for testing purpose', icon: 'https://avatars.githubusercontent.com/u/63374020?v=4' });
+                    // Close the notification
+                    n.click = () => {
+                        if (document.visibilityState !== "visible") {
+                            // If the page is not visible, we want to focus it
+                            // so the user can see the result of the notification
+                            window.parent.focus();
+                            notification.close();
                         }
-
-                        if (i === 9) {
-                            i++;
-                            clearInterval(interval);
-                        }
-                    }, 200);
+                    }
                 } else {
                     // Otherwise, we can fallback to a regular modal alert
                     alert("Notification permission not granted");
